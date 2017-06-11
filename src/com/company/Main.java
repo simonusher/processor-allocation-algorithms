1786309ad76267a56e30a57caeae1be88e21e523
Igor <swozniak6@gmail.com>
2017-06-11 23:49:30 +0200

    final touch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
+    public static int numberOfProcessors = 100;
+    public static int maxTime = 300;
+    public static int maxLoad = 80;
+    public static int minLoad = 20;
+    public static int maxNumberOfQueries = 5;
 
     public static void main(String[] args) {
-        ArrayList<Processor> processors = Generator.generateProcessors(100);
-        Generator.generateProcesses(processors, 300);
-        FirstAlgorithm fa = new FirstAlgorithm(processors, 80, 5);
-        SecondAlgorithm sa = new SecondAlgorithm(processors, 80);
-        ThirdAlgorithm ta = new ThirdAlgorithm(processors, 80, 40);
+        ArrayList<Processor> processors = Generator.generateProcessors(numberOfProcessors);
+        Generator.generateProcesses(processors, maxTime);
+        FirstAlgorithm fa = new FirstAlgorithm(processors, maxLoad, maxNumberOfQueries);
+        SecondAlgorithm sa = new SecondAlgorithm(processors, maxLoad);
+        ThirdAlgorithm ta = new ThirdAlgorithm(processors, maxLoad, minLoad);
         fa.handleProcessors();
         sa.handleProcessors();
         ta.handleProcessors();
